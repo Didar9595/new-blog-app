91a74bac5e6a3534676a01bad3f6cbce19039107
Didar Abbas <didarabbas8@gmail.com>
2024-12-16 20:05:31 +0530

    Header Created

---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+'use client'
+import { Button, Navbar, TextInput } from "flowbite-react"
+import Link from "next/link"
+import { AiOutlineSearch } from "react-icons/ai";
+import {FaMoon} from 'react-icons/fa'
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const path =usePathname();
+  return (
+    <Navbar className="bg-slate-100 px-4">
+        <Link href='/' className="flex flex-row items-center gap-2 font-bold text-lg sm:text-xl dark:text-white">
+          <span className="py-2 px-6 sm:px-2 bg-gradient-to-r from-pink-300 to-orange-400 rounded-lg">DA's</span>
+          BlogSite
+        </Link>
+        <form>
+            <TextInput type="text" placeholder="Search..." rightIcon={AiOutlineSearch} className="hidden lg:inline"/>
+        </form>
+        <Button className="w-10 lg:hidden" color="gray" pill><AiOutlineSearch/></Button>
+        <Navbar.Collapse>
+          <Link href='/'>
+           <Navbar.Link active={path==='/'} as={'div'}>Home</Navbar.Link>
+          </Link>
+          <Link href='/about'>
+           <Navbar.Link active={path==='/about'} as={'div'}>About</Navbar.Link>
+          </Link>
+          <Link href='/projects'>
+           <Navbar.Link active={path==='/projects'} as={'div'}>Projects</Navbar.Link>
+          </Link>
+        </Navbar.Collapse>
+        <div className="flex gap-4">
+            <Button className="w-10" color="gray" pill>
+                <FaMoon/>
+            </Button>
+            <Link href='/sign-in'>
+              <Button gradientDuoTone="pinkToOrange" outline>Sign In</Button>
+            </Link>
+            <Navbar.Toggle className="bg-gray-100"/>
+        </div>
+    </Navbar>
+  )
+}
+
+export default Header
