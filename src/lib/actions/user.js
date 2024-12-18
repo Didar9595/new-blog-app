d7dd3f0ef3dd51a2d0c5171e9218f1cb04277e59
Didar Abbas <didarabbas8@gmail.com>
2024-12-18 18:54:43 +0530

    fix error in action's user.js

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,4 +1,4 @@
-import {User} from '../models/user.model'
+import User from '../models/user.model'
 import {connect} from '../mongodb/mongoose'
 
 export const createOrUpdateUser=async(id,first_name,last_name,image_url,email_addresses,username)=>{
