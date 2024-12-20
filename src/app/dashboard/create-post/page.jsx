5dce7c1aedba483f4ebfe4ad65e7919cf565a64e
Didar Abbas <didarabbas8@gmail.com>
2024-12-20 18:10:25 +0530

    Completed creating new post and adding to mongodb

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -1,23 +1,103 @@
 'use client'
+import { app } from "@/firebase";
 import { useUser } from "@clerk/nextjs"
-import { TextInput, Select, FileInput, Button } from "flowbite-react";
+import { TextInput, Select, FileInput, Button, Alert } from "flowbite-react";
 import dynamic from "next/dynamic";
-const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+import { useState } from "react";
 import 'react-quill-new/dist/quill.snow.css'
+const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import {  CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { useRouter } from "next/navigation";
 
-export default function Page(){
+
+export default function Page() {
+    const [file, setFile] = useState(null);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const [publishError,setPublishError]=useState(null);
+    const router=useRouter()
+    
+    
+    console.log(formData)
+
+
+    const handleUploadImage = () => {
+        try {
+            if (!file) {
+                setImageUploadError("Pls Select an Image to Upload");
+                return;
+            }
+            setImageUploadError(null);
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + '-' + file.name;
+            const storageRef = ref(storage, `new-blog-app/${fileName}`);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            console.log(uploadTask)
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setImageUploadProgress(progress.toFixed(0));
+                },
+                (error) => {
+                    setImageUploadError('Image Upload Failed');
+                    setImageUploadProgress(null);
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setImageUploadProgress(null);
+                        setImageUploadError(null);
+                        setFormData({...formData,image:downloadURL})
+                    })
+                }
+            )
+        } catch (error) {
+            setImageUploadError("Image upload Failed");
+            setImageUploadProgress(null);
+            console.log(error)
+        }
+    }
+
+    const handleSubmit=async()=>{
+        try {
+            const res= await fetch('/api/post/create',{
+                method:"POST",
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({
+                    ...formData,userMongoId:user.publicMetadata.userMongoId
+                }),
+            });
+        const data=await res.json();
+        if(!res.ok){
+            setPublishError(data.message)
+            return
+        }
+        if(res.ok){
+            setPublishError(null)
+            router.push(`/post/${data.slug}`)
+        }
+        } catch (error) {
+            setPublishError("Error Publishing the Post...")
+        }
+    }
+
     const { isSignedIn, user, isLoaded } = useUser();
     if (!isLoaded) {
-        return null;
+        return <div>Loading...</div>;
     }
-    if (isSignedIn && user.publicMetadata.isAdmin) {
+    if (isSignedIn && user?.publicMetadata?.isAdmin) {
         return (
             <div className="flex justify-center items-center">
                 <div className="border my-5 w-[90%] lg:w-[50%] shadow-lg rounded-lg min-h-screen">
                     <h1 className="font-bold text-2xl text-white bg-gradient-to-r from-pink-500 to-orange-400 p-3 text-center rounded-lg">Create a Post</h1>
                     <div className="flex flex-col lg:flex-row items-center justify-center gap-2 my-6">
-                        <TextInput placeholder="Enter title of the post..." type="text" id="title" required className="w-[100%] lg:w-[70%] p-3" />
-                        <Select id="category" required>
+                        <TextInput placeholder="Enter title of the post..." type="text" id="title" required className="w-[100%] lg:w-[70%] p-3" onChange={(e)=>setFormData({...formData,title:e.target.value})}/>
+                        <Select id="category" required onChange={e=>setFormData({...formData,category:e.target.value})}>
                             <option value="technology">Technology</option>
                             <option value="travel">Travel</option>
                             <option value="vlog">Vlog</option>
@@ -27,14 +107,31 @@ export default function Page(){
                         </Select>
                     </div>
                     <div className="p-3">
-                        <div className="p-3 flex justify-between items-center border-2 border-dotted border-pink-400">
-                            <FileInput type='file' accept="image/*" />
-                            <Button gradientDuoTone="pinkToOrange" type="button" outline>Upload Image</Button>
+                        <div className="p-3 flex justify-between items-center gap-3 border-2 border-dotted border-pink-500">
+                            <FileInput type='file' accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
+                            <Button gradientDuoTone="pinkToOrange" type="button" outline onClick={handleUploadImage} className=" text-xs" disabled={imageUploadError}>
+                                {imageUploadProgress?(
+                                    <div className="w-[3em] lg:w-[5em] h-[fit-content]">
+                                        <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress}%`} />
+                                    </div>
+                                ):('Upload')}
+                            </Button>
                         </div>
+                        {
+                            imageUploadError && (
+                                <Alert color="failure" className="my-3">{imageUploadError}</Alert>
+                            )
+                        }
+                        {
+                            formData.image && (
+                                <img src={formData.image} alt="Post Image" className="w-[100%] my-3"/>
+                            )
+                        }
                     </div>
-                    <ReactQuill theme="snow" placeholder="Enter your Text..." required className="h-72 p-3 mb-14 text-white" />
+
+                    <ReactQuill theme="snow" placeholder="Enter your Text..." required className="h-72 p-3 mb-14 text-white" onChange={value=>setFormData({...formData,content:value})}/>
                     <div className="p-3">
-                        <Button gradientDuoTone="pinkToOrange" className="w-[100%] tracking-wider text-xl" type="submit">Post</Button>
+                        <Button gradientDuoTone="pinkToOrange" className="w-[100%] tracking-wider text-xl" type="submit" onClick={handleSubmit}>Post</Button>
                     </div>
                 </div>
 
@@ -42,7 +139,7 @@ export default function Page(){
         )
     }
     else {
-        return <div className="text-2xl font-bold text-red-600 italic text-center py-8">Your are not authorized to to access the page...</div>
+        return <div className="text-2xl font-bold text-red-600 italic text-center py-8">Your are not authorized to access the page...</div>
     }
 
 }
