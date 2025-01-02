fd595fc31243178a7c4cc849c7d6640b7836aa48
Didar Abbas <didarabbas8@gmail.com>
2025-01-02 19:59:35 +0530

    enhancement-3

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -5,9 +5,10 @@ export default function PostCard({post}) {
   return (
       <Card
       className="w-[20rem]"
-      renderImage={() => <img src={post.image} alt="post image"  className="w-[full] h-[300px]"/>}
+      renderImage={() => <img src={post.image} alt="post image"  className="w-[full] h-[200px]"/>}
     >
-      <h5 className="text-2xl font-bold  text-gray-900 dark:text-white">
+     
+     <h5 className="text-2xl font-bold text-gray-900 dark:text-white">
         {post.title}
       </h5>
       <p className="border border-orange-400 p-1 w-[fit-content] rounded-md">
@@ -15,6 +16,7 @@ export default function PostCard({post}) {
       </p>
       <p className="text-gray-600 dark:text-gray-500">Posted By: <span className="text-black dark:text-white italic capitalize">{post.name}</span></p>
       <Link href={`/post/${post.slug}`}><Button gradientDuoTone="pinkToOrange" >Read Post</Button></Link>
+    
     </Card>
   )
 }
